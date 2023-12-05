9ec44ae3822613410f157900f61f76a196f294d9
rstp-god <cruzenepro@yandex.ru>
2023-12-05 05:14:54 +0300

    add response for command

--- src/bot.ts
+++ src/bot.ts
@@ -1,61 +1,57 @@
-import { REST, Routes, Client, GatewayIntentBits } from "discord.js";
+import { REST, Routes, Client, GatewayIntentBits, ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 import * as env from 'dotenv';
-import { Axios } from "axios";
-import { HttpsProxyAgent } from "https-proxy-agent";
+import { ChatApi } from "./service";
 
 env.config();
 
-// const commands = [
-//     {
-//         name: 'ping',
-//         description: 'Replies with Pong!',
-//     },
-// ];
-//
-// // @ts-ignore
-// const rest = new REST({version: "10"}).setToken(process.env.ACCESS_TOKEN)
-//
-// try {
-//     console.log('Started refreshing application (/) commands.');
-//
-//     // @ts-ignore
-//     await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {body: commands});
-//
-//     console.log('Successfully reloaded application (/) commands.');
-// } catch (error) {
-//     console.error(error);
-// }
-//
-// const client = new Client({intents: [GatewayIntentBits.Guilds]});
-//
-// client.on('ready', () => {
-//     // @ts-ignore
-//     console.log(`Logged in as ${client.user.tag}!`);
-// });
-//
-// client.on('interactionCreate', async interaction => {
-//     if (!interaction.isChatInputCommand()) return;
-//
-//     if (interaction.commandName === 'ping') {
-//         await interaction.reply('Pong!');
-//     }
-// });
-//
-// client.login(process.env.ACCESS_TOKEN);
+const api = new ChatApi();
 
+const data = new SlashCommandBuilder()
+    .setName("ask")
+    .setDescription("AskGpt")
+    .addStringOption(option =>
+        option
+            .setName("input")
+            .setDescription("Your question")
+            .setRequired(true));
 
-async function test() {
-    await axios.post("", {
-        model: "gpt-3.5-turbo",
-        messages: [{"role": "user", "content": "Расскажи анекдот"}],
-        temperature: 0.7
-    }).then((result) => {
-        console.log(result.data.choices[0]);
-    }).catch((error) => {
-        console.error(error)
-    })
+const commands = [];
 
+commands.push(data.toJSON());
+
+// @ts-ignore
+const rest = new REST({version: "10"}).setToken(process.env.ACCESS_TOKEN)
+
+try {
+    console.log('Started refreshing application (/) commands.');
+
+    // @ts-ignore
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {body: commands});
+
+    console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+    console.error(error);
 }
 
-// test();
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.on('ready', () => {
+    // @ts-ignore
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'ask') {
+        await interaction.deferReply();
+        await api.getGptAnswer(
+            String(interaction.options.data[0].value)
+        ).then(async (result) => {
+            await interaction.editReply(result);
+        });
+    }
+});
+
+client.login(process.env.ACCESS_TOKEN);
 
--- src/service.ts
+++ src/service.ts
@@ -1,7 +1,8 @@
 import { Axios } from "axios";
 import { HttpsProxyAgent } from "https-proxy-agent";
-
-class ChatApi {
+import * as env from 'dotenv';
+env.config();
+export class ChatApi {
     private axios: Axios;
 
 
@@ -14,11 +15,21 @@ class ChatApi {
             },
             maxBodyLength: Infinity,
             withCredentials: true,
-            httpsAgent: new HttpsProxyAgent(process.env.PROXY_STRING),
+            httpsAgent: new HttpsProxyAgent(process.env.PROXY_STRING!),
             transformResponse: res => JSON.parse(res),
             transformRequest: req => JSON.stringify(req),
         })
     }
 
-
+    public async getGptAnswer(question: string) {
+        return await this.axios.post("", {
+            model: "gpt-3.5-turbo",
+            messages: [{"role": "user", "content": question}],
+            temperature: 0.7
+        }).then((result) => {
+            return result.data.choices[0].message.content;
+        }).catch((error) => {
+            console.error(error)
+        })
+    }
 }
